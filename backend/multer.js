1c8a9b421a54573959980f508c9b1268e3578920
Talha Raza <talha.raza@eventcombo.com>
2025-09-15 18:43:24 +0500

    done

--- backend/multer.js
+++ backend/multer.js
@@ -1,16 +1,21 @@
 const path = require("path");
+const fs = require("fs");
 const multer = require("multer");
 
+const uploadDir = path.join("/tmp", "uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
-  destination: function (req, resp, cb) {
-    cb(null, "uploads/");
+  destination: function (req, file, cb) {
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "_" + Math.round(Math.random() * 1e9);
-    const ext = path.extname(file.originalname); // get extension, e.g., '.png'
-    const filename = file.originalname
-      .replace(ext, "")
-      .replace(/\s+/g, "_"); // replace spaces with underscores
+    const ext = path.extname(file.originalname);
+    const filename = file.originalname.replace(ext, "").replace(/\s+/g, "_");
     cb(null, `${filename}_${uniqueSuffix}${ext}`);
   },
 });
