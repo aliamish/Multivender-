44590fa387b7294f8cb2cfb7fa9d664a4d28441e
Talha Raza <talha.raza@eventcombo.com>
2025-09-15 19:23:58 +0500

    done

--- frontend/src/components/shop/DashboardMessages.jsx
+++ frontend/src/components/shop/DashboardMessages.jsx
@@ -9,7 +9,7 @@ import socketIO from "socket.io-client";
 import { format } from "timeago.js";
 import styles from "../../styles/style";
 
-const ENDPOINT = "http://localhost:4000/";
+const ENDPOINT = "https://socket-ecommerce-tu68.onrender.com/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const DashboardMessages = () => {
--- frontend/src/pages/UserInbox.jsx
+++ frontend/src/pages/UserInbox.jsx
@@ -10,7 +10,7 @@ import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
 import { TfiGallery } from "react-icons/tfi";
 import styles from "../styles/style";
 
-const ENDPOINT = "http://localhost:4000/";
+const ENDPOINT = "https://socket-ecommerce-tu68.onrender.com/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const UserInbox = () => {
